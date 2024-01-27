cfd2cb0622b5d0c0fa899455d834470067575c01
Anish Sherigar <143006940+anishdevadiga@users.noreply.github.com>
2024-01-27 23:12:48 +0530

    Add files via upload

---
+++ String_conversion.dart
@@ -0,0 +1,14 @@
+void main() 
+{
+ //int to string 
+  int a=10;
+  String  b=a.toString(); 
+  print(b);//toString() used to convert int to string
+  print(b.runtimeType);  //to check the data type during runtime
+  
+  //double to string
+  double c=10.233;
+  String d=c.toString();
+  print(d);
+   print(d.runtimeType);
+}
---
+++ String_to_int.dart
@@ -0,0 +1,8 @@
+void main() 
+{
+ //string to int
+  int a=10;
+  int b=int.parse("20");
+  print(a*b);  
+}
+  
